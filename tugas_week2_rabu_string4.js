64478ac16914b3996c491a207e52bd3e612442a5
Muhamad Fadhil Surya Putra <47266686+fadhilsurya@users.noreply.github.com>
2019-03-30 02:38:11 +0700

    Add files via upload

---
+++ tugas_week2_rabu_string4.js
@@ -0,0 +1,18 @@
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word4.substring(0, 3);
+var examplesecondWord4 = word4.substring(4, 14);
+var examplethirdWord4 = word4.substring(15, 17);
+var examplefourthWord4 = word4.substring(18, 20);
+var examplefifthWord4 = word4.substring(20, 25);
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWord4Length = examplesecondWord4.length;
+var thirdWord4Length = examplethirdWord4.length;
+var fourthWord4Length = examplefourthWord4.length;
+var fifthWord4Length = examplefifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + examplesecondWord4 + ', with length: ' + secondWord4Length);
+console.log('Third Word: ' + examplethirdWord4 + ', with length: ' + thirdWord4Length);
+console.log('Fourth Word: ' + examplefourthWord4 + ', with length: ' + fourthWord4Length);
+console.log('Fifth Word: ' + examplefifthWord4 + ', with length: ' + fifthWord4Length);
